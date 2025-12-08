3e4d6bd7744c1c781c2e6101ea6ffafb32e40c13
Nam G. VU <namgivu@gmail.com>
2025-12-08 22:37:00 +0700

    xoá dòng </pre> truoc khi them

--- crud_dslop/add.sh
+++ crud_dslop/add.sh
@@ -1,1 +1,7 @@
+# xoá dòng </pre> truoc khi them
+sed -i '' '/<\/pre>/d' ./dslop.html
+#                  /d de xoa cho enh sed
+#   -i inplace la sed vao tep tructiep taicho ; ko co -i se in ketqua ra
+#   -i '' de chay dc tren macbook va linux
+
 echo 'Ten MOI' >> ./dslop.html

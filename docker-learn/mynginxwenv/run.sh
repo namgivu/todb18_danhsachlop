85ad511eb47cfb5d4804af9ffc0c7192a5599611
Nam G. VU <namgivu@gmail.com>
2025-12-19 22:40:36 +0700

    mynginx w name as envvar via --build-arg k=v @ $1

--- docker-learn/mynginxwenv/run.sh
+++ docker-learn/mynginxwenv/run.sh
@@ -1,5 +1,10 @@
 set -e
     SH=`cd $(dirname $0) && pwd`
 cd $SH
-docker build                  -t mynginxwenv-i .  --no-cache  --build-arg name_a_build_arg='Abb'
+docker build                  -t mynginxwenv-i .  --no-cache  --build-arg name_a_build_arg="$1"
 docker run -e NAME="$1" -p880:80 mynginxwenv-i
+
+_='
+--- whatsnext cmd
+curl localhost:880
+'

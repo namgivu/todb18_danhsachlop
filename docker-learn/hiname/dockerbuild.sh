5cc7f24721c61814c08dab60d7696536e55742aa
Nam G. VU <namgivu@gmail.com>
2025-12-19 20:24:39 +0700

    hiname

--- docker-learn/hiname/dockerbuild.sh
+++ docker-learn/hiname/dockerbuild.sh
@@ -1,3 +1,10 @@
-SH=$(cd `dirname ${BASH_SOURCE:-$0}` && pwd)
+SH=$(cd `dirname ${BASH_SOURCE:-$0}` && pwd)  # lay SH thumuc chua tep .sh nay
 cd $SH
 docker build -t hiname_i .
+#            -t tagname  . context folder
+#                        . app source code folder
+
+echo
+cat <<EOT
+docker image ls | grep -E 'TAG|hiname_i'
+EOT

1183717ca8c2528a9c649d83c3e148d42be32cc6
Nam G. VU <namgivu@gmail.com>
2025-12-19 21:01:37 +0700

    spacing

--- docker-learn/hiname_py/rdockerbuild.sh
+++ docker-learn/hiname_py/rdockerbuild.sh
@@ -1,8 +1,8 @@
 SH=$(cd `dirname ${BASH_SOURCE:-$0}` && pwd)  # lay SH thumuc chua tep .sh nay
 cd $SH
 docker build -t hiname-py-i .
-#            -t tagname  . context folder
-#                        . app source code folder
+#            -t tagname     . context folder
+#                           . app source code folder
 
 echo
 cat <<EOT
